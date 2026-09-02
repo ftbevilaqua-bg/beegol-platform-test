75055ed83e87fd02c9fa895345f5531a9397fc88
Thiago Bevilaqua <francisco.bevilaqua@beegol.com>
2026-09-02 15:21:37 -0300

    feat: remove routes and load db for execute test in cloud

--- server/main.py
+++ server/main.py
@@ -3,17 +3,17 @@ import logging
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from server.auth.router import router as auth_router
-from server.profile.router import router as profile_router
-from server.users.router import router as users_router
-from server.customers.routers.customers import router as customer_router
-from server.customers.routers.messages import router as customer_messages_router
+# from server.auth.router import router as auth_router
+# from server.profile.router import router as profile_router
+# from server.users.router import router as users_router
+# from server.customers.routers.customers import router as customer_router
+# from server.customers.routers.messages import router as customer_messages_router
 from server.core.config import settings
-from server.core.database import Base, engine
+# from server.core.database import Base, engine
 
 logging.basicConfig(level=logging.INFO)
 
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Beegol Platform API")
 
@@ -25,11 +25,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(auth_router)
-app.include_router(profile_router)
-app.include_router(users_router)
-app.include_router(customer_router)
-app.include_router(customer_messages_router)
+# app.include_router(auth_router)
+# app.include_router(profile_router)
+# app.include_router(users_router)
+# app.include_router(customer_router)
+# app.include_router(customer_messages_router)
 
 
 @app.get("/", tags=["health"])
